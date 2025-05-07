fbf8166de5c402d5a3bdb8252d157031e1eb3dbd
Vinay Bhat <vinaybhathg11@gmail.com>
2025-05-07 23:23:38 +0530

    feat: navBar
    move search to navbar
    basic layout of navBar

---
+++ applyflow/src/app/components/navBar/NavBar.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo, useState } from "react";
+import { useJobStore } from "../../appStore";
+
+const NavBar = () => {
+  const jobs = useJobStore((s) => s.jobs);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e?.target?.value);
+  };
+
+  const filteredJobs = useMemo(() => {
+    return jobs.filter((job) =>
+      job.companyName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [searchQuery, jobs]);
+
+  return (
+    <nav className="flex items-center justify-between w-[100%] h-14 shadow-amber-50 shadow-xs p-6">
+      <span className="">Apply Flow</span>
+
+      <div>
+        <input
+          type="text"
+          className="border-[1px] border-amber-50 rounded-md w-96 h-10 p-2"
+          placeholder="search by company name"
+          value={searchQuery}
+          onChange={handleSearchQuery}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- applyflow/src/app/page.tsx
+++ applyflow/src/app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import AddJobDetails from "./components/addJobDetails/AddJobDetails";
 import { useJobStore } from "./appStore";
 import JobBoard from "./components/jobBoard/JobBoard";
+import NavBar from "./components/navBar/NavBar";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
   const addJob = useJobStore((state) => state.addJob);
-  const jobs = useJobStore((s) => s.jobs);
-
-  const handleSearchQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e?.target?.value);
-  };
 
   const handleAddJob = (formData: {
     jobName: string;
@@ -32,40 +27,26 @@ export default function Home() {
     });
   };
 
-  const filteredJobs = useMemo(() => {
-    return jobs.filter((job) =>
-      job.companyName.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-  }, [searchQuery, jobs]);
-
   return (
-    <div className="p-6 w-[100%] flex flex-col gap-2">
-      <div>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="flex px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-        >
-          Add Job
-        </button>
-      </div>
+    <div className="w-[100%] flex flex-col gap-2">
+      <NavBar />
+      <div className="flex flex-col gap-4 p-6">
+        <div>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="flex px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+          >
+            Add Job
+          </button>
+        </div>
 
-      <div>
-        <input
-          type="text"
-          className="border-[1px] border-amber-50 rounded-md w-[400px] h-12 p-2"
-          placeholder="search by company name"
-          value={searchQuery}
-          onChange={handleSearchQuery}
+        <AddJobDetails
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSubmit={handleAddJob}
         />
+        <JobBoard />
       </div>
-
-      <AddJobDetails
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSubmit={handleAddJob}
-      />
-
-      <JobBoard />
     </div>
   );
 }
