bd157f472e1aa78e058370937392f31ee4c8feb7
Vinay Bhat <vinaybhathg11@gmail.com>
2025-05-23 23:10:49 +0530

    fix: css layout for more options

--- applyflow/src/app/components/jobBoard/MoreOption.tsx
+++ applyflow/src/app/components/jobBoard/MoreOption.tsx
@@ -7,20 +7,20 @@ interface JobActionsMenuProps {
 
 const MoreOption: React.FC<JobActionsMenuProps> = ({ onDelete, onEdit }) => {
   return (
-    <div className="absolute left-[40%] top-[12%] right-0 mt-2 w-44 bg-yellow-50 border border-gray-100 rounded-lg shadow-xl z-10 text-black">
+    <div className="absolute left-[60%] top-[12%] right-0 mt-2 w-43 bg-[#374151] border-[#f44336] border-[1px] rounded-lg shadow-xl z-10 text-black">
       <button
-        className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-800 text-sm"
+        className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-[#71717a] text-sm text-[#f44336] cursor-pointer"
         onClick={onEdit}
       >
         Edit
       </button>
       <button
-        className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-800 text-sm text-red-600"
+        className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-[#71717a] text-sm text-[#f44336] cursor-pointer"
         onClick={onDelete}
       >
         Delete
       </button>
-      <button className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-800 text-sm">
+      <button className="w-full px-4 py-2 text-left hover:bg-gray-100 dark:hover:bg-[#71717a] text-sm text-[#f44336] cursor-pointer">
         AI Suggestions
       </button>
     </div>
